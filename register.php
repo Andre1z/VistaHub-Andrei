781cc2fd60584359259d54f960d636594937b28b
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-10 13:29:54 +0100

    estilo para registro

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" type="text/css" href="css/register.css">
 </head>
 <body>
     <h1>Registro de Usuarios</h1>
