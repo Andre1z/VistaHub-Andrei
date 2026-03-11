32f306c3e4e87020526d74eabb377f1299081c0d
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-11 12:10:39 +0100

    creacion de index

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Hub</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+</head>
+<body>
+    <div class="form-container">
+        <img src="assets/vistahub.png" alt="50x50" height="100" width="250">
+        <h2>Iniciar sesión</h2>
+            <p class="error-message"></p>
+            <form method="POST">
+            <input type="email" name="username" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Contraseña" required>
+            <button type="submit">Entrar</button>
+        </form>
+        <p>¿No tiene cuenta? <a href="register.php">Regístrate aquí</a></p>
+    </div>
+</body>
+</html>
