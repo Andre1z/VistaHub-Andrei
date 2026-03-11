b8aa36b00afea04c132c109fdd175004e1a197cd
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-11 13:40:37 +0100

    estilo pagina principal

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="form-container">
         <button><img src="assets/almacen.svg" alt="Almacén">Almacen</button>
         <button><img src="assets/camion.svg" alt="Logistica">Logistica</button>
-        <button><img src="assets/compras.svg" alt="Compras">Compras</button>
+        <button><img src="assets/compras.svg" alt="Compras">Compras</button><br>
         <button><img src="assets/empleados.svg" alt="Empleados">Empleados</button>
         <button><img src="assets/tienda.svg" alt="Tienda">Tienda</button>
         <button><img src="assets/ventas.svg" alt="Ventas">Ventas</button>
