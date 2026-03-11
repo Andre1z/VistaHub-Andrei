ced7b7a177146d19c5f0c5001ad0797c8be04505
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-11 12:41:00 +0100

    imagenes añadidas

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>Vista Hub</title>
 </head>
 <body>
-    
+    <h1>Te damos la Bienvenida a Vista Hub</h1>
+    <h2>Menú Principal</h2>
+    <div class="form-container">
+        <button><img src="assets/almacen.svg" alt="Almacén">Almacen</button>
+    </div>
 </body>
 </html>
