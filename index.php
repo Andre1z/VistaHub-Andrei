33ade0be3a5ff055b0989b9fb124d364b979f63b
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-13 12:17:41 +0100

    importamos main desde un archivo externo

--- index.php
+++ index.php
@@ -13,14 +13,7 @@
     <h1>Te damos la Bienvenida a Vista Hub</h1>
     <h2>Menú Principal</h2>
     <div class="flex">
-        <div class="form-container">
-            <button id="almacen"><img src="assets/almacen.svg" alt="Almacén">Almacen</button>
-            <button id="logistica"><img src="assets/camion.svg" alt="Logistica">Logistica</button>
-            <button id="compras"><img src="assets/compras.svg" alt="Compras">Compras</button><br>
-            <button id="empleados"><img src="assets/empleados.svg" alt="Empleados">Empleados</button>
-            <button id="tienda"><img src="assets/tienda.svg" alt="Tienda">Tienda</button>
-            <button id="ventas"><img src="assets/ventas.svg" alt="Ventas">Ventas</button>
-        </div>
+        <?php include 'main.php'; ?>
         <?php include 'footer.php'; ?>
     </div>
 </body>
---
+++ main.php
@@ -0,0 +1,8 @@
+<div class="form-container">
+    <button id="almacen"><img src="assets/almacen.svg" alt="Almacén">Almacen</button>
+    <button id="logistica"><img src="assets/camion.svg" alt="Logistica">Logistica</button>
+    <button id="compras"><img src="assets/compras.svg" alt="Compras">Compras</button><br>
+    <button id="empleados"><img src="assets/empleados.svg" alt="Empleados">Empleados</button>
+    <button id="tienda"><img src="assets/tienda.svg" alt="Tienda">Tienda</button>
+    <button id="ventas"><img src="assets/ventas.svg" alt="Ventas">Ventas</button>
+</div>
