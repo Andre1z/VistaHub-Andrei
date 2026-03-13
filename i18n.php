7beeed39d8deb8e1db23659778b6f6f6a5643c02
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-13 12:41:10 +0100

    cambiar idioma

---
+++ i18n.php
@@ -0,0 +1,44 @@
+<?php
+function obtenerTraducciones($idioma = 'en') {
+    $traducciones = array();
+    // Se intenta abrir el archivo CSV con las traducciones.
+    if (($archivo = fopen("translations.csv", "r")) !== false) {
+        // Se lee la primera línea para descartar los encabezados
+        $cabecera = fgetcsv($archivo, 1000, ",");
+        // Se procesan cada línea del archivo
+        while (($linea = fgetcsv($archivo, 1000, ",")) !== false) {
+            $clave = $linea[0];
+            $traducciones[$clave] = array(
+                'en' => $linea[1],
+                'es' => $linea[2],
+                'fr' => $linea[3],
+                'de' => $linea[4],
+                'ru' => $linea[5],
+                'pt' => $linea[6],
+                'it' => $linea[7],
+                'pl' => $linea[8],
+                'el' => $linea[9],
+                'ar' => $linea[10]
+            );
+        }
+        fclose($archivo);
+    }
+    return $traducciones;
+}
+
+// Determina el idioma activo a partir de la sesión; si no está definido, se usa inglés ('en').
+if (isset($_SESSION['language'])) {
+    $idiomaActivo = $_SESSION['language'];
+} else {
+    $idiomaActivo = 'en';
+}
+$GLOBALS['idiomaActivo'] = $idiomaActivo;
+$GLOBALS['conjuntoTraducciones'] = obtenerTraducciones($idiomaActivo);
+
+function __($clave) {
+    if (isset($GLOBALS['conjuntoTraducciones'][$clave][$GLOBALS['idiomaActivo']])) {
+        return $GLOBALS['conjuntoTraducciones'][$clave][$GLOBALS['idiomaActivo']];
+    }
+    return isset($GLOBALS['conjuntoTraducciones'][$clave]['en']) ? $GLOBALS['conjuntoTraducciones'][$clave]['en'] : $clave;
+}
+?>
