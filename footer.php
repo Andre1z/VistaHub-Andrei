f5ed2e9b5ffe019a97c520e7a0914282de712ac5
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-13 12:15:35 +0100

    pasamos el footer a un archivo externo

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <menu>
+        <section id="seccion1">
+            <h3>Contacto</h3>
+            <img src="assets/vistahub.png" alt="vistahub" height="65px" width="165px">
+            <span class="fi fi-es"></span>
+            <span class="fi fi-eu"></span>
+            <p>Email: info@vistahub.com</p>
+            <p>Teléfono: +34 91 123 45 67</p>
+            <p>Dirección: <br>C/ Ejemplo, 123 - 28001 Madrid</p>
+            <p>Horario de Atención: <br>Lunes a Viernes, 9:00 - 18:00</p>
+        </section>
+        <section id="seccion2">
+            <h3>Redes Sociales</h3>
+            <div class ="seccion2_0">
+                <img src="assets/facebook.svg" alt="Facebook" height="40px" width="40px">
+                <a href="https://www.facebook.com/VistaHub290" target="_blank"><p>@VistaHub290</p></a>
+            </div>
+            <div class ="seccion2_0">
+                <img src="assets/twitter.svg" alt="Twitter" height="40px" width="40px">
+                <a href="https://twitter.com/VistaHub290" target="_blank"><p>@VistaHub290</p></a>
+            </div>
+            <div class ="seccion2_0">
+                <img src="assets/linkedin.svg" alt="LinkedIn" height="40px" width="40px">
+                <a href="https://www.linkedin.com/company/vistahub290" target="_blank"><p>@VistaHub290</p></a>
+            </div>
+        </section>
+        <section id="seccion3">
+            <h3>Aviso Legal</h3>
+            <p>© 2026 VistaHub. Todos los derechos reservados. El contenido de este sitio web es propiedad de VistaHub y está protegido por las leyes de propiedad intelectual. Queda prohibida la reproducción total o parcial sin autorización previa. VistaHub no se hace responsable por el uso indebido de la información aquí contenida. Para consultas legales o privacidad, contáctanos</p>
+        </section>
+        <section id="seccion4">
+            <h3>Seleccion de Idiomas</h3>
+            <button><span class="fi fi-gb"></span></button>
+            <button><span class="fi fi-fr"></span></button>
+            <button><span class="fi fi-es"></span></button>
+            <button><span class="fi fi-pt"></span></button>
+            <button><span class="fi fi-de"></span></button>
+            <button><span class="fi fi-it"></span></button>
+            <button><span class="fi fi-pl"></span></button>
+            <button><span class="fi fi-gr"></span></button>
+            <button><span class="fi fi-ru"></span></button>
+            <button><span class="fi fi-sa"></span></button>
+        </section>
+    </menu>
+</footer>
