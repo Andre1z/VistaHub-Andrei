9b3f2a4710dd5b7a5d4cdd47f781f84891166803
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-23 11:59:45 +0100

    funcion de botones para idiomas

---
+++ scr/footer.js
@@ -0,0 +1,31 @@
+// Metodos usados para cambiar el idioma de la pagina, redirigiendo a la misma con el parametro lang
+document.getElementById('gb').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=en';
+});
+document.getElementById('es').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=es';
+});
+document.getElementById('fr').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=fr';
+});
+document.getElementById('de').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=de';
+});
+document.getElementById('it').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=it';
+});
+document.getElementById('pt').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=pt';
+});
+document.getElementById('ru').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=ru';
+});
+document.getElementById('pl').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=pl';
+});
+document.getElementById('gr').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=gr';
+});
+document.getElementById('sa').addEventListener('click', function() {
+    window.location.href = 'index.php?lang=sa';
+});
