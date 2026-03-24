0e9c0435f2ff90ef8c6b91f31f32b0bca8d84a1a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-24 12:52:22 +0100

    creacion del header

--- almacen.php
+++ almacen.php
@@ -11,6 +11,19 @@
     <link rel="icon" type="image/png" href="assets/logo.png">
 </head>
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="almacen.php">Almacen</a></li>
+                <li><a href="logistica.php">Logistica</a></li>
+                <li><a href="empleados.php">Empleados</a></li>
+                <li><a href="compras.php">Compras</a></li>
+                <li><a href="ventas.php">Ventas</a></li>
+                <li><a href="tienda.php">Tienda</a></li>
+            </ul>
+        </nav>
+    </header>
     <h2>Almacen</h2>
     <?php include 'footer.php'; ?>
 </body>
