f0e39d7a5f2c34889b647536a5b9123c23ce2837
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-24 12:24:00 +0100

    almacen actualizado

---
+++ almacen.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Hub - Almacen</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+</head>
+<body>
+    <h2>Almacen</h2>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
