1ba2f271d7b3ddb5f846d31d46ed17e49c10870a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-24 12:15:23 +0100

    js para los botones

--- scr/main.js
+++ scr/main.js
@@ -1,3 +1,18 @@
 document.getElementById('almacen').addEventListener('click', function() {
     window.location.href = 'almacen.php';
 });
+document.getElementById('logistica').addEventListener('click', function() {
+    window.location.href = 'logistica.php';
+});
+document.getElementById('compras').addEventListener('click', function() {
+    window.location.href = 'compras.php';
+});
+document.getElementById('ventas').addEventListener('click', function() {
+    window.location.href = 'ventas.php';
+});
+document.getElementById('empleados').addEventListener('click', function() {
+    window.location.href = 'empleados.php';
+});
+document.getElementById('tienda').addEventListener('click', function() {
+    window.location.href = 'tienda.php';
+});
