78b64148f720abf06d5346d4122616aaa6f2078a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-26 12:44:58 +0100

    separacion del header e include

--- almacen.php
+++ almacen.php
@@ -12,19 +12,7 @@
     <link rel="stylesheet" type="text/css" href="css/header.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="almacen.php">Almacen</a></li>
-                <li><a href="logistica.php">Logistica</a></li>
-                <li><a href="empleados.php">Empleados</a></li>
-                <li><a href="compras.php">Compras</a></li>
-                <li><a href="ventas.php">Ventas</a></li>
-                <li><a href="tienda.php">Tienda</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php include 'header.php'; ?>
     <h2>Almacen</h2>
     <?php include 'footer.php'; ?>
 </body>
---
+++ header.php
@@ -0,0 +1,14 @@
+<link rel ="stylesheet" type="text/css" href="css/header.css">
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="almacen.php">Almacen</a></li>
+            <li><a href="logistica.php">Logistica</a></li>
+            <li><a href="empleados.php">Empleados</a></li>
+            <li><a href="compras.php">Compras</a></li>
+            <li><a href="ventas.php">Ventas</a></li>
+            <li><a href="tienda.php">Tienda</a></li>
+        </ul>
+    </nav>
+</header>
