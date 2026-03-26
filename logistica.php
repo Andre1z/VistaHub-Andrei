7655a279b2dc750973d9a089687f95050e9487d7
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-26 12:43:01 +0100

    cambios en logistica

---
+++ logistica.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Hub - Logistica</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<body>
+    <?php include 'header.php'; include 'footer.php';?>
+</body>
+</html>
