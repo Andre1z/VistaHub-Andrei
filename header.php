051cfd570821c0cdba6c744ae103904c31b8c634
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-26 13:31:39 +0100

    cambios en el header

--- header.php
+++ header.php
@@ -6,9 +6,13 @@
             <li><a href="almacen.php">Almacen</a></li>
             <li><a href="logistica.php">Logistica</a></li>
             <li><a href="empleados.php">Empleados</a></li>
+            <li><a href="clientes.php">Clientes</a></li>
             <li><a href="compras.php">Compras</a></li>
             <li><a href="ventas.php">Ventas</a></li>
             <li><a href="tienda.php">Tienda</a></li>
+            <li><a href="productos.php">Productos</a></li>
+            <li><a href="manual.php">Manual de Usuario</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
         </ul>
     </nav>
 </header>
