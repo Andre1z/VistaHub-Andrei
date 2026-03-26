e5b64a964e098b9e025039b999664b37b796e5fd
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-26 12:49:55 +0100

    cambios en ventas

---
+++ ventas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Hub - Ventas</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <h2>Ventas</h2>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
