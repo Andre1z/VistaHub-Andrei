6ffdefb5ae39eb8738ae3dd8e350e2ff99f34c4b
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-26 12:46:42 +0100

    cambios con el orden y estilo

--- logistica.php
+++ logistica.php
@@ -6,10 +6,10 @@
     <title>Vista Hub - Logistica</title>
     <link rel="stylesheet" type="text/css" href="css/general.css">
     <link rel="icon" type="image/png" href="assets/logo.png">
-    <link rel="stylesheet" type="text/css" href="css/header.css">
-    <link rel="stylesheet" type="text/css" href="css/footer.css">
 </head>
 <body>
-    <?php include 'header.php'; include 'footer.php';?>
+    <?php include 'header.php'; ?>
+    <h2>Logistica</h2>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
