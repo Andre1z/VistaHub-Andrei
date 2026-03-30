d81a96876e3581e3e05e8502dab59698489e4e16
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-30 09:13:48 +0200

    iframe añadido a contacto

--- contacto.php
+++ contacto.php
@@ -11,6 +11,7 @@
 <body>
     <?php include 'header.php'; ?>    
     <h2>Contacto</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3461.565052654528!2d-3.6912047905093273!3d40.42990609448063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd42288da0e82c27%3A0xeb1c1e744f61fd3f!2sC.%20de%20Caracas%2C%2021%2C%20Chamber%C3%AD%2C%2028010%20Madrid!5e1!3m2!1ses!2ses!4v1774854629621!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  
     <form action="contacto_procesar.php" method="post" class="contact-form">
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" required>
