c46784d306e7085f2c156ce7646b45620487aa4c
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-30 11:56:09 +0200

    cambios en main

--- main.php
+++ main.php
@@ -6,6 +6,9 @@
     <button id="tienda"><img src="assets/tienda.svg" alt="Tienda">Tienda</button>
     <button id="ventas"><img src="assets/ventas.svg" alt="Ventas">Ventas</button><br>
     <button id="productos"><img src="assets/productos.svg" alt="Productos">Productos</button>
-    <button id="clientes"><img src="assets/clientes.svg" alt="Clientes">Clientes</button>
+    <button id="proveedores"><img src="assets/proveedores.svg" alt="Proveedores">Proveedores</button>
+    <button id="clientes"><img src="assets/clientes.svg" alt="Clientes">Clientes</button><br>
     <button id="acercade"><img src="assets/acercade.svg" alt="Acerca de">Acerca de</button>
+    <button id="manual"><img src="assets/manual.svg" alt="Manual">Manual</button>
+    <button id="salir"><img src="assets/salir.svg" alt="Salir">Salir</button>
 </div>
