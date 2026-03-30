52b992a26f36c8a1dd63d3d7a365d14ec0e23cd3
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-30 10:39:07 +0200

    cambio estetico en main

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
     <button id="empleados"><img src="assets/empleados.svg" alt="Empleados">Empleados</button>
     <button id="tienda"><img src="assets/tienda.svg" alt="Tienda">Tienda</button>
     <button id="ventas"><img src="assets/ventas.svg" alt="Ventas">Ventas</button><br>
-    <button id="clientes"><img src="assets/clientes.svg" alt="Clientes">Clientes</button>
-    <button id="manual"><img src="assets/manual.svg" alt="Manual de Usuario">Manual de Usuario</button>
     <button id="productos"><img src="assets/productos.svg" alt="Productos">Productos</button>
+    <button id="clientes"><img src="assets/clientes.svg" alt="Clientes">Clientes</button>
+    <button id="acercade"><img src="assets/acercade.svg" alt="Acerca de">Acerca de</button>
 </div>
