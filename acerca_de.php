b6c27f92f1d3affe81c1bcfa5bef2ce46422af51
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-30 09:56:28 +0200

    cambios en acerca de

--- acerca_de.php
+++ acerca_de.php
@@ -11,10 +11,13 @@
     <?php include 'header.php'; ?>
     <main>
         <h2>Acerca de Vista Hub</h2>
-        <p>Vista Hub es una plataforma de gestión de proyectos diseñada para ayudar a equipos a colaborar de manera eficiente y organizada. Nuestra misión es proporcionar una herramienta intuitiva y poderosa que facilite la planificación, seguimiento y ejecución de proyectos de cualquier tamaño.</p>
-        <p>Con Vista Hub, puedes crear proyectos, asignar tareas, establecer plazos y monitorear el progreso en tiempo real. Nuestra plataforma está diseñada para adaptarse a las necesidades de equipos de todos los tamaños, desde pequeñas startups hasta grandes corporaciones.</p>
-        <p>En Vista Hub, creemos que la colaboración es clave para el éxito de cualquier proyecto. Por eso, hemos creado una plataforma que fomenta la comunicación abierta y la transparencia entre los miembros del equipo. Con nuestras herramientas de colaboración integradas, puedes compartir archivos, discutir ideas y mantener a todos en la misma página.</p>
-        <p>Estamos comprometidos a brindar un excelente servicio al cliente y a mejorar continuamente nuestra plataforma para satisfacer las necesidades cambiantes de nuestros usuarios. Si tienes alguna pregunta o sugerencia, no dudes en ponerte en contacto con nuestro equipo de soporte.</p>
+        <p>Vista Hub es un CRM (Customer Relationship Management) diseñado para ayudar a las empresas a gestionar sus relaciones con los clientes de manera eficiente y efectiva.</p>
+        <p>Nuestra misión es proporcionar una plataforma intuitiva y poderosa que permita a las empresas mejorar la comunicación con sus clientes, aumentar la productividad y, en última instancia, impulsar el crecimiento del negocio.</p>
+        <p>En Vista Hub, creemos que la gestión de las relaciones con los clientes es fundamental para el éxito de cualquier empresa. Por eso, nos esforzamos por ofrecer una solución que sea fácil de usar, personalizable y que se adapte a las necesidades específicas de cada negocio.</p>
+        <p>Tenemos la sede principal en Madrid, España. Aquí teneis el mapa de nuestra ubicación:</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3461.565052654528!2d-3.6912047905093273!3d40.42990609448063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd42288da0e82c27%3A0xeb1c1e744f61fd3f!2sC.%20de%20Caracas%2C%2021%2C%20Chamber%C3%AD%2C%2028010%20Madrid!5e1!3m2!1ses!2ses!4v1774854629621!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <p>En caso de que tenga alguna pregunta, no dude en contactarnos a través de la sección de <a href="contacto.php">contacto</a>.</p>
+    </main>
     <?php include 'footer.php'; ?>
 </body>
 </html>
