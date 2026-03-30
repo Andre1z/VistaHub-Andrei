bd468afa275934c24437a41f9437e8fa89ee449e
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-03-30 12:44:56 +0200

    contenido añadido para el manual de usuario

---
+++ manual.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Hub - Manual de Usuario</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+</head>
+<body>
+    <h2>Manual de Usuario</h2>
+</body>
+</html>
