79731d3741b599b0bc24f82e78808552f57fac59
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-14 13:18:07 +0200

    ponemos include para importar el contenido

--- manual.php
+++ manual.php
@@ -12,61 +12,7 @@
     <h2>Manual de Usuario</h2>
     <p>Te damos la bienvenida al manual de usuario de Vista Hub. Donde en esta pagina encontraras información sobre cómo utilizar nuestra plataforma.</p>
     <br>
-    <h3>1. Registro e Inicio de Sesión</h3>
-    <p>Para comenzar a utilizar Vista Hub, primero debes registrarte. Haz clic en el botón "Registrarse" en la esquina superior derecha de la página de inicio. Completa el formulario con los datos requeridos. Una vez enviado el formulario se debera esperar un rato para que se validen los datos y le enviemos un correo con la contraseña temporal despues, podrá iniciar sesión con tus credenciales.</p>
-    <img src="assets/registro_aqui.png" alt="Registro" height="600px" width="390px">
-    <img src="assets/registro_campos1.png" alt="" height="600px" width="420px">
-    <img src="assets/registro_campos2.png" alt="" height="600px" width="420px">
-    <br>
-    <h3>2. Navegación por la página principal</h3>
-    <p>Una vez que hayas iniciado sesión, serás dirigido a tu panel de control. Aquí podrás acceder a todas las funcionalidades de Vista Hub, como la gestión de sus productos, la gestion de su flota y la configuración de su tienda.</p>
-    <img src="assets/menu_principal1.png" alt="">
-    <img src="assets/menu_principal2.png" alt="">
-    <br>
-    <h3>3. Gestión de Almacén</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tu almacén de manera eficiente.</p>
-    <img src="assets/almacen_mantenimiento.png" alt="">
-    <br>
-    <h3>4. Gestión de Logística</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tu flota de manera eficiente.</p>
-    <img src="assets/logistica_mantenimiento.png" alt="">
-    <br>
-    <h3>5. Gestión de los empleados</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus empleados de manera eficiente.</p>
-    <img src="assets/empleados_mantenimiento.png" alt="">
-    <br>
-    <h3>6. Gestión de Clientes</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus clientes de manera eficiente.</p>
-    <img src="assets/clientes_mantenimiento.png" alt="">
-    <br>
-    <h3>7. Gestión de Compras</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tus compras de manera eficiente.</p>
-    <img src="assets/compras_mantenimiento.png" alt="">
-    <br>
-    <h3>8. Gestión de Ventas</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tus ventas de manera eficiente.</p>
-    <img src="assets/ventas_mantenimiento.png" alt="">
-    <br>
-    <h3>9. Configuración de la Tienda</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder configurar tu tienda de manera eficiente.</p>
-    <img src="assets/tienda_mantenimiento.png" alt="">
-    <br>
-    <h3>10. Inventario de Productos</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar el inventario de tus productos de manera eficiente.</p>
-    <img src="assets/productos_mantenimiento.png" alt="">
-    <br>
-    <h3>11. Proveedores</h3>
-    <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus proveedores de manera eficiente.</p>
-    <img src="assets/proveedores_mantenimiento.png" alt="">
-    <br>
-    <h3>12. Acerca de Vista Hub</h3>
-    <p>En esta sección podrás encontrar información sobre la empresa y su misión.</p>
-    <img src="assets/acerca_de.png" alt="">
-    <br>
-    <h3>13. Contacto</h3>
-    <p>En esta sección podrá contactar con el equipo de soporte de Vista Hub mediante el formulario de la misma pagina.</p>
-    <img src="assets/contacto.png" alt="">
-    <br>
+    <?php include 'manual_contenido.php'; ?>
     <?php include 'footer.php'; ?>
 </body>
 </html>
