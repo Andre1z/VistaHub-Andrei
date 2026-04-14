2dbbaf2cff1bd9848f506f0278098384ca454721
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-14 12:18:24 +0200

    manual de usuario primer apartado

--- manual.php
+++ manual.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,15 @@
     <link rel="icon" type="image/png" href="assets/logo.png">
 </head>
 <body>
+    <?php include 'header.php'; ?>
     <h2>Manual de Usuario</h2>
+    <p>Te damos la bienvenida al manual de usuario de Vista Hub. Donde en esta pagina encontraras información sobre cómo utilizar nuestra plataforma.</p>
+    <br>
+    <h3>1. Registro e Inicio de Sesión</h3>
+    <p>Para comenzar a utilizar Vista Hub, primero debes registrarte. Haz clic en el botón "Registrarse" en la esquina superior derecha de la página de inicio. Completa el formulario con los datos requeridos. Una vez enviado el formulario se debera esperar un rato para que se validen los datos y le enviemos un correo con la contraseña temporal despues, podrá iniciar sesión con tus credenciales.</p>
+    <img src="assets/registro_aqui.png" alt="Registro" height="600px" width="390px">
+    <img src="assets/registro_campos1.png" alt="" height="600px" width="390px">
+    <img src="assets/registro_campos2.png" alt="" height="600px" width="390px">
+    <?php include 'footer.php'; ?>
 </body>
 </html>
