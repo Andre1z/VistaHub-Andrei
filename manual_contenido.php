737e495b49a5c1d0788320b45da6a91338202c2b
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-14 13:30:42 +0200

    numeracion para el manual de usuario rectificada

--- manual_contenido.php
+++ manual_contenido.php
@@ -17,47 +17,47 @@
 <p>En el pie de pagina encontraras información de la empresa, redes sociales de la empresa, el aviso legal y un selector de idiomas.</p>
 <img src="assets/pie_de_pagina.png" alt="">
 <br>
-<h3>3. Gestión de Almacén</h3>
+<h3>5. Gestión de Almacén</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tu almacén de manera eficiente.</p>
 <img src="assets/almacen_mantenimiento.png" alt="">
 <br>
-<h3>4. Gestión de Logística</h3>
+<h3>6. Gestión de Logística</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tu flota de manera eficiente.</p>
 <img src="assets/logistica_mantenimiento.png" alt="">
 <br>
-<h3>5. Gestión de los empleados</h3>
+<h3>7. Gestión de los empleados</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus empleados de manera eficiente.</p>
 <img src="assets/empleados_mantenimiento.png" alt="">
 <br>
-<h3>6. Gestión de Clientes</h3>
+<h3>8. Gestión de Clientes</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus clientes de manera eficiente.</p>
 <img src="assets/clientes_mantenimiento.png" alt="">
 <br>
-<h3>7. Gestión de Compras</h3>
+<h3>9. Gestión de Compras</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tus compras de manera eficiente.</p>
 <img src="assets/compras_mantenimiento.png" alt="">
 <br>
-<h3>8. Gestión de Ventas</h3>
+<h3>10. Gestión de Ventas</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar tus ventas de manera eficiente.</p>
 <img src="assets/ventas_mantenimiento.png" alt="">
 <br>
-<h3>9. Configuración de la Tienda</h3>
+<h3>11. Configuración de la Tienda</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder configurar tu tienda de manera eficiente.</p>
 <img src="assets/tienda_mantenimiento.png" alt="">
 <br>
-<h3>10. Inventario de Productos</h3>
+<h3>12. Inventario de Productos</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar el inventario de tus productos de manera eficiente.</p>
 <img src="assets/productos_mantenimiento.png" alt="">
 <br>
-<h3>11. Proveedores</h3>
+<h3>13. Proveedores</h3>
 <p>En esta sección tendrás a disposición todas las herramientas necesarias para poder gestionar a tus proveedores de manera eficiente.</p>
 <img src="assets/proveedores_mantenimiento.png" alt="">
 <br>
-<h3>12. Acerca de Vista Hub</h3>
+<h3>14. Acerca de Vista Hub</h3>
 <p>En esta sección podrás encontrar información sobre la empresa y su misión.</p>
 <img src="assets/acerca_de.png" alt="">
 <br>
-<h3>13. Contacto</h3>
+<h3>15. Contacto</h3>
 <p>En esta sección podrá contactar con el equipo de soporte de Vista Hub mediante el formulario de la misma pagina.</p>
 <img src="assets/contacto.png" alt="">
 <br>
