2d322db25dbc35d19a28e3f6851dd7cd9e43b33a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-15 11:51:31 +0200

    Cambios en el css

--- manual.php
+++ manual.php
@@ -5,14 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista Hub - Manual de Usuario</title>
     <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="stylesheet" type="text/css" href="css/manual.css">
     <link rel="icon" type="image/png" href="assets/logo.png">
 </head>
 <body>
     <?php include 'header.php'; ?>
-    <h2>Manual de Usuario</h2>
-    <p>Te damos la bienvenida al manual de usuario de Vista Hub. Donde en esta pagina encontraras información sobre cómo utilizar nuestra plataforma.</p>
-    <br>
-    <?php include 'manual_contenido.php'; ?>
+    <main class="manual-content">
+        <h2>Manual de Usuario</h2>
+        <p>Te damos la bienvenida al manual de usuario de Vista Hub. Donde en esta pagina encontraras información sobre cómo utilizar nuestra plataforma.</p>
+        <?php include 'manual_contenido.php'; ?>
+    </main>
     <?php include 'footer.php'; ?>
 </body>
 </html>
