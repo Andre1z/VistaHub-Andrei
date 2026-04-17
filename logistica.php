50fa5e6c9b529bdafe8f5f7c7755d6de3f1e3565
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-17 13:33:53 +0200

    Update logistica.php

--- logistica.php
+++ logistica.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vista Hub - Logistica</title>
+    <title>Vista Hub - Logística</title>
     <link rel="stylesheet" type="text/css" href="css/general.css">
     <link rel="icon" type="image/png" href="assets/logo.png">
 </head>
