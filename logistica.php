ca2660b851a306384ed92cca4aeac5bfc292f45a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-04-24 13:20:12 +0200

    cambios en la logica de los botones

--- logistica.php
+++ logistica.php
@@ -50,8 +50,8 @@
                         <td><?php echo $row['observaciones']; ?></td>
                         <td><?php echo $row['incidencias']; ?></td>
                         <td>
-                            <button class="editar-btn" onclick="abrirModalEditarProducto(<?php echo $row['id']; ?>)"><img src="assets/editar.svg" alt="<?php echo __('edit'); ?>"></button>
-                            <button class="eliminar-btn" onclick="return confirm('<?php echo __('delete_confirm'); ?>') && eliminarProducto(<?php echo $row['id']; ?>)"><img src="assets/eliminar.svg" alt="<?php echo __('delete'); ?>"></button>
+                            <button class="editar-btn" onclick="abrirModalEditarLogistica(<?php echo $row['id']; ?>)"><img src="assets/editar.svg" alt="<?php echo __('edit'); ?>"></button>
+                            <button class="eliminar-btn" onclick="return confirm('<?php echo __('delete_confirm'); ?>') && eliminarLogistica(<?php echo $row['id']; ?>)"><img src="assets/eliminar.svg" alt="<?php echo __('delete'); ?>"></button>
                         </td>
                     </tr>
             <?php endwhile; ?>
